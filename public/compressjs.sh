700cea431aec0071884f01ab71880d653b13b2fe
Milo van der Linden <miblon@dogodigi.net>
2014-03-10 23:54:09 +0100

    Licentie verwijzing en copyright text aan alle broncode toegevoegd

--- public/compressjs.sh
+++ public/compressjs.sh
@@ -1,41 +1,55 @@
-cat js/libs/jquery-1.10.2.min.js \
+#
+#  Copyright (c) 2014 Milo van der Linden (milo@dogodigi.net)
+# 
+#  This file is part of safetymapDBK
+#  
+#  safetymapDBK is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  safetymapDBK is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with safetymapDBK. If not, see <http://www.gnu.org/licenses/>.
+ 
+ 
+cat js/libs/jquery-1.11.0/jquery-1.11.0.js \
     js/libs/jquery.pagination.js \
     js/libs/jquery.drags.js \
     js/libs/jquery.sortable.js \
-    js/libs/jquery.xml2json.js \
+    js/libs/jquery.daterangepicker.js \
+    js/libs/jquery.typeahead.js \
     js/libs/proj4js-compressed.js \
     js/libs/OpenLayers.dbk.js \
-    js/libs/bootstrap.min.js \
-    js/libs/typeahead.min.js \
-    js/libs/moment.min.js \
-    js/libs/lang/nl.js \
-    js/libs/daterangepicker.js \
+    js/libs/bootstrap-3.1.0/js/bootstrap.js \
+    js/libs/moment/moment.min.js \
     js/dbkjs/util.js \
+    js/dbkjs/config/styles.js \
     js/dbkjs/prototype/Class.js \
     js/dbkjs/prototype/Layer.js \
     js/dbkjs/prototype/Capabilities.js \
     js/dbkjs/layout.js \
-    js/dbkjs/ui/gui.js \
     js/dbkjs/modules/geolocate.js \
-    js/dbkjs/protocol/imdbk21.js \
+    js/dbkjs/protocol/jsonDBK.js \
     js/dbkjs/modules/search.js \
-    js/dbkjs/modules/help.js \
+    js/dbkjs/modules/support.js \
     js/dbkjs/modules/print.js \
-    js/dbkjs/modules/object.js \
     js/dbkjs/modules/regio.js \
     js/dbkjs/modules/district.js \
     js/dbkjs/modules/gemeente.js \
-    js/dbkjs/modules/preventie.js \
-    js/dbkjs/modules/preparatie.js \
-    js/dbkjs/modules/gevaren.js \
     js/dbkjs/modules/care.js \
     js/dbkjs/modules/filter.js \
     js/dbkjs/modules/bag.js \
     js/dbkjs/modules/feature.js \
     js/dbkjs/modules/measure.js \
-    js/dbkjs/dbkjs.js | node_modules/uglify-js/bin/uglifyjs -o js/dbk.min.js
+    js/dbkjs/dbkjs.js | node ../node_modules/uglify-js/bin/uglifyjs -o js/dbk.min.js
+
 cat css/dbk.css \
     css/bootstrap.min.css \
     css/font-awesome.min.css \
     css/daterangepicker-bs3.css \
-    css/typeahead.js-bootstrap.css | node node_modules/clean-css/bin/cleancss -o css/dbk.min.css
+    css/typeahead.js-bootstrap.css | node ../node_modules/clean-css/bin/cleancss -o css/dbk.min.css
