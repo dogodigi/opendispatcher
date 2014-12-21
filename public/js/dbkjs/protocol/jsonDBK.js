7de826de20b11c512ad125b874e545177dae1e79
milovanderlinden <milo@dogodigi.net>
2014-12-21 23:20:22 +0100

    fix voor #401

--- public/js/dbkjs/protocol/jsonDBK.js
+++ public/js/dbkjs/protocol/jsonDBK.js
@@ -273,7 +273,7 @@ dbkjs.protocol.jsonDBK = {
         var _obj = dbkjs.protocol.jsonDBK;
         /** Algemene dbk info **/
         var controledatum = dbkjs.util.isJsonNull(DBKObject.controleDatum) ? '<span class="label label-warning">'+
-                i18n.t('dbk.unknown')+ '</span>' : DBKObject.controleDatum;
+                i18n.t('dbk.unknown')+ '</span>' : moment(DBKObject.controleDatum).format('YYYY-MM-DD');
         if (dbkjs.showStatus) {
             var status = dbkjs.util.isJsonNull(DBKObject.status) ? '<span class="label label-warning">'+
                       i18n.t('dbk.unknown')+ '</span>' : DBKObject.status;
