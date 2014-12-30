f252be82abf882ee7734b96280f46ae3c20fbd9e
milovanderlinden <milo@dogodigi.net>
2014-12-31 00:02:03 +0100

    Insert test feature

--- test/server_test.js
+++ test/server_test.js
@@ -2,7 +2,6 @@ var request = require('nodeunit-express');
 var server = require('../server');
 
 var express = request(server.app);
-var testFeature;
 exports["Rest interfaces"] = {
     setUp: function (callback) {
         //insert some stuff in the database?
@@ -33,7 +32,8 @@ exports["Rest interfaces"] = {
             var result = JSON.parse(response.body);
             test.equal(response.statusCode, 200, '/api/features.json is not available');
             test.equal(!result.features, false, 'Features should be returned');
-            testFeature = result.features[0];
+            console.log(result);
+            var testFeature = result.features[0];
             test.equal(typeof(testFeature), "object","Test Feature should be an object");
             test.equal(testFeature.type, "Feature","Test Feature should have \"type\" set to \"feature\"");
             test.equal(typeof(testFeature.geometry), "object","Test Feature needs to have geometry");
