6aa2391629c7b615ba44aa52c4241cbb4e750eb9
milovanderlinden <milo@dogodigi.net>
2015-01-02 13:05:54 +0100

    Added esting for object and gebied

--- test/server_test.js
+++ test/server_test.js
@@ -35,10 +35,42 @@ exports["Rest interfaces"] = {
             console.log(result);
             var testFeature = result.features[0];
             if(testFeature){
+                test.equal(typeof(testFeature), "object","Single row from Features should be an object");
+                test.equal(testFeature.type, "Feature","Single row from Features should have \"type\" set to \"feature\"");
+                test.equal(typeof(testFeature.geometry), "object","Single row from Features needs to have geometry");
+                test.equal(typeof(testFeature.properties), "object","Single row from Features needs to have properties");
+            } else {
                 test.equal(typeof(testFeature), "object","Test Feature should be an object");
-                test.equal(testFeature.type, "Feature","Test Feature should have \"type\" set to \"feature\"");
-                test.equal(typeof(testFeature.geometry), "object","Test Feature needs to have geometry");
-                test.equal(typeof(testFeature.properties), "object","Test Feature needs to have properties");
+            }
+            test.done();
+        });
+    },
+    "/api/object/1.json": function (test) {
+        express.get('/api/object/1.json').expect(function (response) {
+            // response is the response from hitting '/'
+            var result = JSON.parse(response.body);
+            test.equal(response.statusCode, 200, '/api/object/1.json is not available');
+            test.equal(!result.DBKObject, false, 'Should return A DBKObject');
+            var testFeature = result.DBKObject;
+            if(testFeature){
+                test.equal(typeof(testFeature), "object","Test Feature should be an object");
+                test.equal(testFeature.identificatie, 1,"identifier should be 1");
+            } else {
+                test.equal(typeof(testFeature), "object","Test Feature should be an object");
+            }
+            test.done();
+        });
+    },
+    "/api/gebied/2.json": function (test) {
+        express.get('/api/gebied/2.json').expect(function (response) {
+            // response is the response from hitting '/'
+            var result = JSON.parse(response.body);
+            test.equal(response.statusCode, 200, '/api/object/1.json is not available');
+            test.equal(!result.DBKGebied, false, 'Should return A DBKGebied');
+            var testFeature = result.DBKGebied;
+            if(testFeature){
+                test.equal(typeof(testFeature), "object","Test Feature should be an object");
+                test.equal(testFeature.identificatie, 2,"identifier should be 2");
             } else {
                 test.equal(typeof(testFeature), "object","Test Feature should be an object");
             }
