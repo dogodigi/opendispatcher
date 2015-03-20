ec9c806147b2a7b3293f6eb8afb0e77034c7cdf6
escheper <Eddy.Scheper@aris.nl>
2015-03-20 14:24:39 +0100

    verwijder console.log

--- public/js/dbkjs/config/styles.js
+++ public/js/dbkjs/config/styles.js
@@ -56,7 +56,6 @@ dbkjs.scaleStyleValue = function(value, featureAttributeValue, attributeScaleFac
 dbkjs.config.styleUtils = {
     gevaarlijkestofLabel: function(f) {
         var a = f.attributes;
-        console.log(a);
         var s = (a.information !== "0" && a.information !== "" ? a.information + "\n": "" );
         if(a.indication || a.quantity || a.name) {
             s += "(";
@@ -79,7 +78,7 @@ dbkjs.config.styleUtils = {
         }
         return s;
     }
-}
+};
 
 dbkjs.config.styles = {
     dbkfeature: new OpenLayers.StyleMap({
