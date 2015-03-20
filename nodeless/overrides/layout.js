db10cebe9f5db44dc307c6582be4806a3f9978ab
escheper <Eddy.Scheper@aris.nl>
2015-03-20 11:33:45 +0100

    move style scaling options to dbk.options instead of dbk.config, added to options_mobile.js

--- nodeless/overrides/layout.js
+++ nodeless/overrides/layout.js
@@ -2,23 +2,27 @@
 dbkjs.layout.settingsDialog = function(parent) {
     $(parent).append("<h4>" + i18n.t('app.layout') + "</h4>");
     $(parent).append('<p><div class="row"><div class="col-xs-12">' +
-            '<input type="checkbox" id="click_scaleStyle" checked="checked">' + i18n.t('app.scaleStyle') +
+            '<input type="checkbox" id="checkbox_scaleStyle">' + i18n.t('app.scaleStyle') +
             '</div></div></p>' +
             '<p><hr/><div class="row"><div class="col-xs-12">' +
             '<p style="padding-bottom: 15px">' + i18n.t('app.styleSizeAdjust') + '</p>' +
-            '<input id="slider_styleSizeAdjust" style="width: 210px" data-slider-id="styleSizeAdjustSlider" type="text" data-slider-min="-10" ' +
-            'data-slider-max="4" data-slider-step="1" data-slider-value="0" value="0"/>' +
+            '<input id="slider_styleSizeAdjust" style="width: 210px" data-slider-id="styleSizeAdjustSlider" type="text" ' +
+            ' data-slider-min="-4" data-slider-max="10" data-slider-step="1"/>' +
             '</div></div></p><hr>'
             );
 
-    $("#slider_styleSizeAdjust").slider({ tooltip: "always" });
+    $("#slider_styleSizeAdjust").slider({
+        value: dbkjs.options.styleSizeAdjust,
+        tooltip: "always"
+    });
     $("#slider_styleSizeAdjust").on('slide', function(e) {
-        dbkjs.config.styleSizeAdjust = e.value;
+        dbkjs.options.styleSizeAdjust = e.value;
         dbkjs.redrawScaledLayers();
     });
 
-    $("#click_scaleStyle").on('change', function(e) {
-        dbkjs.config.styleScaleAdjust = e.target.checked;
+    $("#checkbox_scaleStyle").prop("checked", dbkjs.options.styleScaleAdjust);
+    $("#checkbox_scaleStyle").on('change', function(e) {
+        dbkjs.options.styleScaleAdjust = e.target.checked;
         dbkjs.redrawScaledLayers();
     });        
 
