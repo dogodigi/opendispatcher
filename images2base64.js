3720086ed574f29d09e72cb2d79e0546056ef559
escheper <Eddy.Scheper@aris.nl>
2015-03-24 08:27:38 +0100

    mogelijkheid om images als base64 JSON te includen zodat ze ook getoond worden als externalGraphic voor vector layer als verbinding is verbroken

---
+++ images2base64.js
@@ -0,0 +1,59 @@
+/**
+ *  Copyright (c) 2014 B3Partners B.V. (info@b3partners.nl)
+ *
+ *  This file is part of safetymapDBK
+ *
+ *  safetymapDBK is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  safetymapDBK is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with safetymapDBK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+var path = require('path');
+var fs = require('fs');
+
+/**
+ * Writes public/js/dbkjs/images_base64.js
+ *
+ * Include the above file to enable inline images, otherwise the regular files will be used.
+ */
+
+var imagesBase64 = {};
+
+var imagesDir = path.resolve(__dirname, "public/images");
+
+var ignore = ["brandweer-nederland_alpha_shaded.png", "brandweer-nederland_alpha_shaded-s.png", "doiv_logo_def.png", "missing.gif",
+    "MNarrow.png", "overview_replacement.gif"];
+
+function convertRecursive(dir, id) {
+	var files = fs.readdirSync(dir);
+
+	for(var f in files) {
+		var fn = files[f];
+		var stats = fs.statSync(dir + '/' + fn);
+		if(stats.isFile()) {
+		    if(ignore.indexOf(fn) == -1) {
+                console.log(id + fn);
+    		    imagesBase64[id + fn] = "data:image/png;base64," + new Buffer(fs.readFileSync(dir + '/' + fn)).toString('base64');
+    		}
+		} else if(stats.isDirectory()) {
+			convertRecursive(dir + '/' + fn , id + fn + '/');
+		}
+	}
+}
+
+convertRecursive(imagesDir, "images/");
+
+fs.writeFileSync(path.resolve(__dirname, 'public/js/dbkjs/images_base64.js'), "var imagesBase64 = " + JSON.stringify(imagesBase64));
+
+
+
--- nodeless/overrides/public/jsonDBK.js
+++ nodeless/overrides/public/jsonDBK.js
@@ -1,4 +1,132 @@
 
+dbkjs.protocol.jsonDBK.constructBrandweervoorziening = function(brandweervoorziening){
+    var _obj = dbkjs.protocol.jsonDBK;
+    if(brandweervoorziening){
+        var id = 'collapse_brandweervoorziening_' + dbkjs.options.feature.identificatie;
+        var bv_div = $('<div class="tab-pane" id="' + id + '"></div>');
+        var bv_table_div = $('<div class="table-responsive"></div>');
+        var bv_table = _obj.constructBrandweervoorzieningHeader();
+        var features = [];
+        $.each(brandweervoorziening, function(idx, myGeometry){
+            var myFeature = new OpenLayers.Feature.Vector(new OpenLayers.Format.GeoJSON().read(myGeometry.geometry, "Geometry"));
+            myFeature.attributes = {
+                "type" : myGeometry.typeVoorziening,
+                "name": myGeometry.naamVoorziening,
+                "information": myGeometry.aanvullendeInformatie,
+                "rotation": myGeometry.hoek,
+                "category": myGeometry.categorie,
+                "namespace": myGeometry.namespace,
+                "radius": myGeometry.radius,
+                "fid": "brandweervoorziening_ft_" + idx
+            };
+            var myrow = _obj.constructBrandweervoorzieningRow(myFeature.attributes);
+            myrow.mouseover(function(){
+                dbkjs.selectControl.select(myFeature);
+            });
+            myrow.mouseout(function(){
+                dbkjs.selectControl.unselect(myFeature);
+            });
+            bv_table.append(myrow);
+            features.push(myFeature);
+
+        });
+        _obj.layerBrandweervoorziening.addFeatures(features);
+        _obj.activateSelect(_obj.layerBrandweervoorziening);
+        bv_table_div.append(bv_table);
+        bv_div.append(bv_table_div);
+        _obj.panel_group.append(bv_div);
+        _obj.panel_tabs.append('<li><a data-toggle="tab" href="#' + id + '">'+ i18n.t('dbk.prevention')+ '</a></li>');
+    }
+};
+    
+dbkjs.protocol.jsonDBK.constructBrandweervoorzieningHeader = function() {
+    var bv_table = $('<table class="table table-hover"></table>');
+        bv_table.append('<tr><th>' +
+                i18n.t('prevention.type') + '</th><th>' +
+                i18n.t('prevention.name') + '</th><th>' +
+                i18n.t('prevention.comment') + '</th></tr>');
+    return bv_table;
+};
+    
+dbkjs.protocol.jsonDBK.constructBrandweervoorzieningRow = function(brandweervoorziening) {
+    var img = "images/" + brandweervoorziening.namespace + '/' +  brandweervoorziening.type + '.png';
+    img = typeof imagesBase64 === 'undefined'  ? dbkjs.basePath + img : imagesBase64[img];
+    return $('<tr>' +
+                '<td><img class="thumb" src="' + img + '" alt="'+
+                    brandweervoorziening.type +'" title="'+
+                    brandweervoorziening.type+'"></td>' +
+                '<td>' + brandweervoorziening.name + '</td>' +
+                '<td>' + brandweervoorziening.information + '</td>' +
+            '</tr>');
+};
+    
+dbkjs.protocol.jsonDBK.constructGevaarlijkestof = function(gevaarlijkestof){
+    var _obj = dbkjs.protocol.jsonDBK;
+    if(gevaarlijkestof){
+        var id = 'collapse_gevaarlijkestof_' + dbkjs.options.feature.identificatie;
+        var bv_div = $('<div class="tab-pane" id="' + id + '"></div>');
+        var bv_table_div = $('<div class="table-responsive"></div>');
+        var bv_table = _obj.constructGevaarlijkestofHeader();
+        var features = [];
+         $.each(gevaarlijkestof, function(idx, myGeometry){
+            var myFeature = new OpenLayers.Feature.Vector(new OpenLayers.Format.GeoJSON().read(myGeometry.geometry, "Geometry"));
+            myFeature.attributes = {
+                "type" : myGeometry.symboolCode,
+                "name": myGeometry.naamStof,
+                "namespace": myGeometry.namespace,
+                "quantity": myGeometry.hoeveelheid ? myGeometry.hoeveelheid.replace(/([0-9]+)([lL])/,"$1\u2113") : myGeometry.hoeveelheid,
+                "indication": myGeometry.gevaarsindicatienummer,
+                "information": myGeometry.aanvullendeInformatie,
+                "unnumber": myGeometry.UNnummer,
+                "fid": "gevaarlijkestof_ft_" + idx
+            };
+            var myrow = _obj.constructGevaarlijkestofRow(myFeature.attributes);
+            myrow.mouseover(function(){
+                dbkjs.selectControl.select(myFeature);
+            });
+            myrow.mouseout(function(){
+                dbkjs.selectControl.unselect(myFeature);
+            });
+            bv_table.append(myrow);
+            features.push(myFeature);
+        });
+        _obj.layerGevaarlijkestof.addFeatures(features);
+        _obj.activateSelect(_obj.layerGevaarlijkestof);
+        bv_table_div.append(bv_table);
+        bv_div.append(bv_table_div);
+        _obj.panel_group.append(bv_div);
+        _obj.panel_tabs.append('<li><a data-toggle="tab" href="#' + id + '">'+ i18n.t('dbk.chemicals')+ '</a></li>');
+    }
+};
+    
+dbkjs.protocol.jsonDBK.constructGevaarlijkestofHeader = function() {
+    var bv_table = $('<table class="table table-hover"></table>');
+        bv_table.append('<tr><th>' +
+            i18n.t('chemicals.type') + '</th><th>' +
+            i18n.t('chemicals.indication') + '</th><th>' +
+            i18n.t('chemicals.name') + '</th><th>' +
+            i18n.t('chemicals.quantity') + '</th><th>' +
+            i18n.t('chemicals.information') + '</th></tr>');
+    return bv_table;
+};
+    
+dbkjs.protocol.jsonDBK.constructGevaarlijkestofRow = function(gevaarlijkestof) {
+    var img = 'images/' + gevaarlijkestof.namespace + '/' +  gevaarlijkestof.type + '.png';
+    img = typeof imagesBase64 === 'undefined'  ? dbkjs.basePath + img : imagesBase64[img];
+    return $('<tr>' +
+        '<td><img class="thumb" src="' + img + '" alt="'+
+            gevaarlijkestof.type +'" title="'+
+            gevaarlijkestof.type+'"></td>' +
+        '<td>' + '<div class="gevicode">' + gevaarlijkestof.indication +
+            '</div><div class="unnummer">' +
+            gevaarlijkestof.unnumber + '</div>' + '</td>' +
+        '<td>' + gevaarlijkestof.name + '</td>' +
+        '<td>' + gevaarlijkestof.quantity + '</td>' +
+
+        '<td>' + gevaarlijkestof.information + '</td>' +
+        '</tr>');
+};
+
 dbkjs.protocol.jsonDBK.getfeatureinfo = function(e){
     $('#vectorclickpanel_h').html('<span class="h4"><i class="icon-info-sign">&nbsp;' + e.feature.layer.name + '</span>');
     if(e.feature.layer.name === 'Gevaarlijke stoffen' || e.feature.layer.name === 'Brandweervoorziening') {
