501251a4cd7f01b5548967b5144a544252b67576
escheper <Eddy.Scheper@aris.nl>
2015-03-29 15:44:02 +0200

    Aanpassen van het support panel

--- public/js/dbkjs/modules/support.js
+++ public/js/dbkjs/modules/support.js
@@ -48,6 +48,7 @@ dbkjs.modules.support = {
                     '</a>' +
                     '</div>');
             var supportpanel = dbkjs.util.createDialog('supportpanel', '<i class="fa fa-envelope-o"></i> ' + dbkjs.options.organisation.support.button, 'bottom:0;left:0;');
+            $('body').append(supportpanel);
             if(dbkjs.viewmode !== 'fullscreen') {
                 $('.dialog').drags({handle: '.panel-heading'});
                 $('.btn-group').drags({handle: '.drag-handle'});
