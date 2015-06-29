5324e01114854701f4cdf23053c4439d7c7bb44b
milovanderlinden <milo@dogodigi.net>
2015-06-29 09:44:43 +0200

    emailer controller synced

--- controllers/emailer.js
+++ controllers/emailer.js
@@ -18,86 +18,63 @@
  *
  */
 
-var nodemailer = require("nodemailer");
-var i18n = require('i18next');
-global.conf = require('nconf');
-global.conf.file({ file: '../config.json' });
+/* global exports, global */
 
-var smtp = nodemailer.createTransport("SMTP",global.conf.get('support:smtp'));
-exports.annotationbulk = function(req, res) {
+/**
+ * 
+ * @param {type} req
+ * @param {type} res
+ * @returns success or error message
+ */
+exports.postAnnotation = function (req, res) {
+    var t = res.locals.t;
+    var nodemailer = require("nodemailer");
+    var smtp = nodemailer.createTransport("SMTP", global.conf.get('support:smtp'));
+    // @todo get email from request, it can be changed in the database.
     email = global.conf.get('support:sendto');
-    locale ='nl';
-    //console.log(email);
-    //console.log(locale);
-    //TODO: Make sure the locale is always picked up from the right directory!
-    i18n.init({
-        lng: locale, 
-        //debug: true,
-        resGetPath: './locales/__lng__/__ns__.json'
-    }, function(t) {
-            var query_str = "select * from organisation.annotation where sent = false";
-            global.pool.query(query_str,
-                function(err, result) {
-                    if (err) {
-                        res.json(err);
-                    } else {
-                        var errors = [];
-                        var success = [];
-                        for (index = 0; index < result.rows.length; ++index) {
-                            
-                            var htmltemplate = t("email.annotationtitle") + ',<br/><br/>' +
-                                //bericht hier
-                                '<table>' + 
-                                        '<tr><th>Melding:</th><td>' + result.rows[index].gid + '</td></tr>' + 
-                                        '<tr><th>Adres:</th><td>' + result.rows[index].address + '</td></tr>' + 
-                                        '<tr><th>Korte omschrijving:</th><td>' + result.rows[index].subject + '</td></tr>' + 
-                                        '<tr><th>Melding:</th><td>' + result.rows[index].remarks + '</td></tr>' + 
-                                        '<tr><td colspan="2"><hr /><td></tr>' + 
-                                        '<tr><th>Melder:</th><td>' + result.rows[index].name + '</td></tr>' + 
-                                        '<tr><th>Email:</th><td>' + result.rows[index].email + '</td></tr>' + 
-                                        '<tr><th>Telefoon:</th><td>' + result.rows[index].phone + '</td></tr>' + 
-                                        '<tr><td colspan="2"><hr /><td></tr>' + 
-                                        '<tr><td colspan="2">Klik op de link om de melding te openen:</td></tr>' + 
-                                        '<tr><td colspan="2"><a href="' + result.rows[index].permalink + '">'  + result.rows[index].permalink + '</td></tr><br/><br/>' + 
-                                t("email.kindregards") + ',<br/><br/>' +
-                                t("email.team");
-                            var plaintemplate = t("email.annotationtitle") + ',\r\n\r\n' +
-                                //bericht hier
-                                        'Melding: ' + result.rows[index].gid + '\r\n' + 
-                                        'Adres: ' + result.rows[index].address + '\r\n' + 
-                                        'Korte omschrijving:\r\n' + result.rows[index].subject + '\r\n' +  
-                                        'Melding: ' + result.rows[index].remarks  + '\r\n' + 
-                                        '-----------------------------------------------------'  + '\r\n' + 
-                                        'Melder: ' + result.rows[index].name  + '\r\n' + 
-                                        'Email: ' + result.rows[index].email  + '\r\n' +  
-                                        'Telefoon: ' + result.rows[index].phone  + '\r\n' +
-                                        '-----------------------------------------------------'  + '\r\n' + 
-                                        'Klik op de link om de melding te openen:' +  '\r\n' +
-                                        result.rows[index].permalink +  '\r\n\r\n' + 
-                                t("email.kindregards") + ',\r\n\r\n' +
-                                t("email.team");
-                            smtp.sendMail({
-                                from: global.conf.get('support:from'),
-                                to: global.conf.get('support:sendto') + ',' + result.rows[index].email,
-                                subject: t("email.annotation"),
-                                text: plaintemplate,
-                                html: htmltemplate,
-                                forceEmbeddedImages: true
-                            }, function(error, response) {
-                                if (error) {
-                                    errors.push({"error": error.message});
-                                    return;
-                                } else {
-                                    success.push({"success": response.message});
-                                    return;
-                                }
-                            });
-                        }
-                        smtp.close();
-                        res.json({"processed": result.rows.length});
-                    }
-                return;
-            });
+    var htmltemplate = t("email.annotationtitle") + ',<br/><br/>' +
+            '<table>' +
+            '<tr><th>' + t("email.title") + '</th><td>' + t("email.new") + '</td></tr>' +
+            '<tr><th>' + t("email.address") + '</th><td>' + req.body.address + '</td></tr>' +
+            '<tr><th>' + t("email.subject") + '</th><td>' + req.body.subject + '</td></tr>' +
+            '<tr><th>' + t("email.remarks") + '</th><td>' + req.body.remarks + '</td></tr>' +
+            '<tr><td colspan="2"><hr /><td></tr>' +
+            '<tr><th>' + t("email.sender") + '</th><td>' + req.body.name + '</td></tr>' +
+            '<tr><th>' + t("email.email") + '</th><td>' + req.body.email + '</td></tr>' +
+            '<tr><th>' + t("email.phone") + '</th><td>' + req.body.phone + '</td></tr>' +
+            '<tr><td colspan="2"><hr /><td></tr>' +
+            '<tr><td colspan="2">' + t("email.link") + '</td></tr>' +
+            '<tr><td colspan="2"><a href="' + req.body.permalink + '">' + req.body.permalink + '</td></tr><br/><br/>' +
+            t("email.kindregards") + ',<br/><br/>' +
+            t("email.team");
+    var plaintemplate = t("email.annotationtitle") + ',\r\n\r\n' +
+            t("email.title") + ': ' + t("email.new") + '\r\n' +
+            t("email.address") + ': ' + req.body.address + '\r\n' +
+            t("email.subject") + ': ' + req.body.subject + '\r\n' +
+            t("email.remarks") + ':\r\n\r\n' + req.body.remarks + '\r\n' +
+            '-----------------------------------------------------' + '\r\n' +
+            t("email.sender") + ': ' + req.body.name + '\r\n' +
+            t("email.email") + ': ' + req.body.email + '\r\n' +
+            t("email.phone") + ': ' + req.body.phone + '\r\n' +
+            '-----------------------------------------------------' + '\r\n' +
+            t("email.link") + ': ' + +'\r\n' +
+            req.body.permalink + '\r\n\r\n' +
+            t("email.kindregards") + ',\r\n\r\n' +
+            t("email.team");
+    smtp.sendMail({
+        from: global.conf.get('support:from'),
+        to: global.conf.get('support:sendto') + ',' + req.body.email,
+        subject: t("email.annotation"),
+        text: plaintemplate,
+        html: htmltemplate,
+        forceEmbeddedImages: true
+    }, function (error, response) {
+        if (error) {
+            res.status(400).json({"status": "error", "message": error.message});
+            return;
+        } else {
+            res.status(200).json({"status": "ok", "message": "sent"});
+            return;
+        }
     });
-
 };
