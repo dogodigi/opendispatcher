860216fae048d65e404809e4a79b3f6259228b45
milovanderlinden <milo@dogodigi.net>
2015-06-29 09:44:06 +0200

    dbkjs controller synced

--- controllers/dbk.js
+++ controllers/dbk.js
@@ -18,16 +18,18 @@
  *
  */
 
+/* global exports, global */
+
 /**
+ * Get the settings for the organisation from the database based on 
+ * the database connection defined in config.json or config.default.json
  * 
  * @param {type} req
  * @param {type} res
- * @returns {json}
+ * @returns organisation settings for the application
  */
 exports.getOrganisation = function(req, res) {
-    //where identificatie = 1369659645
     if (req.query) {
-        id = req.params.id;
         srid = req.query.srid;
         if(!srid){
             srid = 4326;
@@ -36,9 +38,9 @@ exports.getOrganisation = function(req, res) {
         global.pool.query(query_str, [srid],
             function(err, result){
                 if(err) {
-                    res.json(err);
+                    res.status(400).json(err);
                 } else {
-                    res.json(result.rows[0]);
+                    res.json(removeNulls(result.rows[0]));
                 }
                 return;
             }
@@ -46,27 +48,15 @@ exports.getOrganisation = function(req, res) {
     }
 };
 
-exports.postAnnotation = function(req, res) {
-    var point = 'POINT(' + parseFloat(req.body.geometry.coordinates[0]) + ' '+ parseFloat(req.body.geometry.coordinates[1]) + ')';
-    //console.log(point);
-    var query_str = 'insert into organisation.annotation (subject, name, email, '+ 
-        'municipality, place, address, phone, remarks, permalink, the_geom) values ($1, $2, $3, $4, $5, $6, $7, $8, ST_transform(ST_PointFromText($9, $10),4326))';
-    global.pool.query(query_str, [req.body.subject, req.body.name, req.body.email, 
-        req.body.municipality, req.body.place, req.body.address, req.body.phone, req.body.remarks, req.body.permalink, point, req.body.srid],
-        function(err, result){
-            if(err) {
-                res.json(err);
-            } else {
-                res.json({"result":"ok"});
-            }
-            return;
-        }
-    );
-    
-};
-
+/**
+ * Select an object from the database by id. takes srid as parameter. 
+ * If srid is undefined, falls back to WGS84
+ * 
+ * @param {type} req
+ * @param {type} res
+ * @returns object
+ */
 exports.getObject = function(req, res) {
-    //where identificatie = 1369659645
     if (req.query) {
         id = req.params.id;
         srid = req.query.srid;
@@ -77,9 +67,9 @@ exports.getObject = function(req, res) {
         global.pool.query(query_str, [id, srid],
             function(err, result){
                 if(err) {
-                    res.json(err);
+                    res.status(400).json(err);
                 } else {
-                    res.json(result.rows[0]);
+                    res.json(removeNulls(result.rows[0]));
                 }
                 return;
             }
@@ -88,7 +78,6 @@ exports.getObject = function(req, res) {
 };
 
 exports.getGebied = function(req, res) {
-    //where identificatie = 1369659645
     if (req.query) {
         id = req.params.id;
         srid = req.query.srid;
@@ -99,17 +88,17 @@ exports.getGebied = function(req, res) {
         global.pool.query(query_str, [id, srid],
             function(err, result){
                 if(err) {
-                    res.json(err);
+                    res.status(400).json(err);
                 } else {
-                    res.json(result.rows[0]);
+                    res.json(removeNulls(result.rows[0]));
                 }
                 return;
             }
         );
     }
 };
+
 exports.getFeatures = function(req, res) {
-    //where identificatie = 1369659645
     if (req.query) {
         srid = req.query.srid;
         if(!srid){
@@ -119,21 +108,39 @@ exports.getFeatures = function(req, res) {
         global.pool.query(query_str, [srid],
             function(err, result){
                 if(err) {
-                    res.json(err);
+                    res.status(400).json(err);
                 } else {
                     var resultset = {"type": "FeatureCollection", "features": []};
-                    
+
                     for (index = 0; index < result.rows.length; ++index) {
-                        var item = {type: 'Feature', id: 'site.gid-' + result.rows[index].feature.gid};
+                        var item = {type: 'Feature', id: 'DBKFeature.gid--' + result.rows[index].feature.gid};
                         item.geometry = result.rows[index].feature.geometry;
                         item.properties = result.rows[index].feature;
                         delete item.properties.geometry;
                         resultset.features.push(item);
                     }
-                    res.json(resultset);
+                    res.json(removeNulls(resultset));
                 }
                 return;
             }
         );
     }
+};
+
+/**
+ * Compact arrays with null entries; delete keys from objects with null value
+ * 
+ * @param {json} data
+ * @returns data with nulls removed.
+ */
+var removeNulls = function(data){
+  var y;
+  for (var x in data) {
+    y = data[x];
+    if (y==="null" || y===null || y==="" || typeof y === "undefined" || (y instanceof Object && Object.keys(y).length === 0)) {
+      delete data[x];
+    }
+    if (y instanceof Object) y = removeNulls(y);
+  }
+  return data;
 };
