f61c6ae63a81a2396553da33c51a446c9d437748
milovanderlinden <milo@dogodigi.net>
2015-06-29 12:05:55 +0200

    Legend enhancement

--- public/js/dbkjs/dbkjs.js
+++ public/js/dbkjs/dbkjs.js
@@ -196,6 +196,9 @@ dbkjs.loadOrganisationCapabilities = function () {
                 if (!dbkjs.util.isJsonNull(wms_v.pl)) {
                     metadata.pl = wms_v.pl;
                 }
+                if (!dbkjs.util.isJsonNull(wms_v.legend)) {
+                    metadata.legend = wms_v.legend;
+                }
                 var layertype = wms_v.layertype || null;
                 var myLayer = new dbkjs.Layer(
                         wms_v.name,
@@ -218,6 +221,9 @@ dbkjs.loadOrganisationCapabilities = function () {
                 }
                 if (!dbkjs.util.isJsonNull(wms_v.pl)) {
                     metadata.pl = wms_v.pl;
+                }
+                if (!dbkjs.util.isJsonNull(wms_v.legend)) {
+                    metadata.legend = wms_v.legend;
                 }
                 var layertype = wms_v.layertype || null;
                 var myLayer = new dbkjs.Layer(
