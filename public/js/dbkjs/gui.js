8976283d159260adf964087fba46677a69501ace
Milo van der Linden <milo@dogodigi.net>
2015-09-07 19:21:31 +0200

    Update gui.js

--- public/js/dbkjs/gui.js
+++ public/js/dbkjs/gui.js
@@ -17,11 +17,7 @@ dbkjs.gui = {
         $('#btn_refresh').click(function() {
             if (dbkjs.viewmode !== 'fullscreen') {
                $('#btn_refresh > i').addClass('fa-spin');
-<<<<<<< HEAD
             }
-=======
-            };
->>>>>>> upstream/master
             obj.get();
         });
     },
