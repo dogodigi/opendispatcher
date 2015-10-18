f143fe86bd67d9b77f8f86d2b7a07d4aa1f15cb1
milovanderlinden <milo@dogodigi.net>
2015-10-18 13:27:47 +0200

    Prototypes for message feeds

--- public/js/dbkjs/prototype/Class.js
+++ public/js/dbkjs/prototype/Class.js
@@ -20,6 +20,40 @@
 
 var dbkjs = dbkjs || {};
 window.dbkjs = dbkjs;
+dbkjs.Util = dbkjs.Util || {};
+dbkjs.Util.extend = function(destination, source) {
+    destination = destination || {};
+    if (source) {
+        for (var property in source) {
+            var value = source[property];
+            if (value !== undefined) {
+                destination[property] = value;
+            }
+        }
+
+        /**
+         * IE doesn't include the toString property when iterating over an object's
+         * properties with the for(property in object) syntax.  Explicitly check if
+         * the source has its own toString property.
+         */
+
+        /*
+         * FF/Windows < 2.0.0.13 reports "Illegal operation on WrappedNative
+         * prototype object" when calling hawOwnProperty if the source object
+         * is an instance of window.Event.
+         */
+
+        var sourceIsEvt = typeof window.Event == "function" &&
+          source instanceof window.Event;
+
+        if (!sourceIsEvt &&
+          source.hasOwnProperty && source.hasOwnProperty("toString")) {
+            destination.toString = source.toString;
+        }
+    }
+    return destination;
+};
+
 dbkjs.Class = function() {
     var len = arguments.length;
     var P = arguments[0];
---
+++ public/js/dbkjs/prototype/Feed.js
@@ -0,0 +1,83 @@
+/*!
+ *  Copyright (c) 2014 Milo van der Linden (milo@dogodigi.net)
+ *
+ *  This file is part of opendispatcher/safetymapsDBK
+ *
+ *  opendispatcher is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  opendispatcher is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with opendispatcher. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* global OpenLayers */
+
+var dbkjs = dbkjs || {};
+window.dbkjs = dbkjs;
+dbkjs.Feed = dbkjs.Class({
+  events: null,
+  data: null,
+  projection: "EPSG:4326",
+  url: 'http://www.google.nl',
+  reload: 200000,
+  crossDomain: true,
+  dataType: 'xml',
+  error: false,
+  timeout: null,
+  i: 0,
+  initialize: function(name, url, options, callback) {
+    var _obj = this;
+    this.options = dbkjs.Util.extend({}, options);
+    dbkjs.Util.extend(this, options);
+    this.name = name;
+    this.url = url;
+    this.callback = callback;
+    this.load();
+  },
+  triggerTimeout: function(){
+    var _obj = this;
+    this.timeout = setTimeout(function () {
+        _obj.load();
+    }, _obj.reload);
+  },
+  load: function() {
+    var _obj = this;
+    if(_obj.timeout){
+      clearTimeout(_obj.timeout);
+    }
+    _obj.error = false;
+
+    $.ajaxPrefilter(function(options) {
+      if (options.crossDomain) {
+        options.url = 'proxy/?q=' + encodeURIComponent(options.url + '?' + options.data.toString());
+        options.crossDomain = false;
+      }
+    });
+    $.ajax(this.url, {
+        crossDomain: this.crossDomain,
+        data: this.data,
+        dataType: this.dataType
+      }).done(function(response) {
+        //check the datatype of the response. If it is xml, convert to JSON
+        if(_obj.dataType === 'xml'){
+          _obj.triggerTimeout();
+          _obj.callback(_obj.name, JSON.parse(xml2json(response, " ")));
+        } else if (_obj.dataType === 'json'){
+          _obj.triggerTimeout();
+          _obj.callback(_obj.name, response);
+        }
+      })
+      .fail(function() {
+        _obj.callback(_obj.name,{});
+      });
+  },
+  CLASS_NAME: "dbkjs.Feed"
+});
