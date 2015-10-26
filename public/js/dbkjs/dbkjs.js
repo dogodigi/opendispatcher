2d7d33d982d4f2c8a08efd43aaa515de538c3c87
milovanderlinden <milo@dogodigi.net>
2015-10-26 10:38:56 +0100

    Removed whitespace

--- public/js/dbkjs/dbkjs.js
+++ public/js/dbkjs/dbkjs.js
@@ -40,7 +40,7 @@ OpenLayers.IMAGE_RELOAD_ATTEMPTS = 3;
  * @external Proj4js
  */
 
- /**
+/**
  * @memberof external:Proj4js
  * @property {String} EPSG:28992 - <code>+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.999908 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs <></code>
  */
